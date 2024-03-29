55ffc4665d63d1fe67be1b320a0e920ab453c782
bruno <bernardin.bruno@orange.fr>
2024-03-29 15:54:04 +0100

    US1-Lien vers page des POE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { InternFormComponent } from './home/intern-form/intern-form.component';
+import { PoeHomeComponent } from './poe/poe-home/poe-home.component';
+import { AddPoeComponent } from './poe/add-poe/add-poe.component';
 
 const routes: Routes = [
   {
-    path: '', 
-    redirectTo: 'home', 
-    pathMatch: 'full'
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
 
   {
-    path: 'home', 
-    component: HomeComponent, 
-    pathMatch: 'full'
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
   },
   {
-    path: '**', 
+    path: 'intern-add',
+    component: InternFormComponent,
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'poe-home',
+    component: PoeHomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'create-form',
+    component: AddPoeComponent,
+    pathMatch: 'full',
+  },
+
+  {
+    path: '**',
     redirectTo: 'home',
-    pathMatch: 'full'
-  } 
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
